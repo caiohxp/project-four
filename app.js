8145f930754e19a490b4351ec0b9e1cfb1b2520d
caiohxp <caiohxp228@gmail.com>
2022-02-12 05:53:37 -0300

    Website Structure

---
+++ app.js
@@ -0,0 +1,45 @@
+const titleDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    '—',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    'ENTER'
+]
+
+const handleClick = () => {
+    console.log('clicked')
+}
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', handleClick)
+    keyboard.append(buttonElement)
+})
